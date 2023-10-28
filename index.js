9d31204018dd71a223ae3eece51bce1b730468ff
vladimarkova <vladislavam1996@gmail.com>
2023-10-28 11:42:07 +0300

    test.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+console.log(`Setting base dir to ${__dirname}`);
 const exists = fs.existsSync('./dist/src/index.js');
 if (!exists) {
   const message = `No bootstrap file found! Make sure you have successfully compiled the project and ${path.resolve(__dirname, 'dist', 'index.js')} exist!`;
